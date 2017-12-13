02b785c13d3f5c53dff31cdfa1ea389174a650dc
Alberto Mayorga <mayorga.at@gmail.com>
2017-12-13 16:10:37 -0600

    Set up and test MySQL connection

---
+++ api/controllers/UserController.js
@@ -0,0 +1,18 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	getUsers: function(req, res) {
+		User.find().exec(function(err, user) {
+			if(err) {
+				console.log(err);
+			}
+			res.json(user);
+		});
+	}
+};
---
+++ api/models/User.js
@@ -0,0 +1,50 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  tableName: 'usuarios',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  attributes: {
+    idUsuario: {
+      type: 'integer',
+      autoIncrement: true,
+      primaryKey: true,
+      columnName: 'idUsuario'
+    },
+    nombre: {
+      type: 'string',
+      size: 50,
+      columnName: 'nombre'
+    },
+    apellidos: {
+      type: 'string',
+      size: 80,
+      columnName: 'apellidos'
+    },
+    correo: {
+      type: 'string',
+      size: 50,
+      unique: true,
+      columnName: 'correo'
+    },
+    idTipoUsuario: {
+      type: 'integer',
+      columnName: 'idTipoUsuario'
+    },
+    idTipoEstado: {
+      type: 'integer',
+      columnName: 'idTipoEstado'
+    },
+    password: {
+      type: 'string',
+      size: 100,
+      columnName: 'password'
+    }
+  }
+
+};
